2e6df592c55461892cf8bbfa1aa8c12fba4505b6
Ken Ong <ken.ong@dius.com.au>
2017-07-19 18:39:55 +1000

    Initial gradle springboot project

---
+++ src/test/groovy/com/care2compare/search/SearchApplicationTests.groovy
@@ -0,0 +1,16 @@
+package com.care2compare.search
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner)
+@SpringBootTest
+class SearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
