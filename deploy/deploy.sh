934c87b3b3e8c45cfa10b3993f7e783a69a133d5
Ken Ong <kong@dius.com.au>
2017-11-27 12:24:00 +1100

    Added dpeloyment templates and scripts for assets

---
+++ deploy/deploy.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -euxo pipefail
+
+docker run --rm \
+    -v "`pwd`:/cwd" \
+    -e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY -e AWS_SESSION_TOKEN \
+    -e AWS_DEFAULT_REGION \
+    realestate/stackup:1.2.0 care-to-compare-assets up \
+    -t /cwd/deploy/sis-store.yml \
+    -o DomainName=caretocompare.com \
+    -o FullDomainName=assets.caretocompare.com
