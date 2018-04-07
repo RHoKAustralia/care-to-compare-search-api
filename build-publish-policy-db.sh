64e9a363d99105d18d60cc942cb9efe756503351
Ken Ong <kenming.ong@railslice.com>
2018-04-07 10:40:40 +1000

    update with cleanup of docker containers

--- build-publish-policy-db.sh
+++ build-publish-policy-db.sh
@@ -2,7 +2,9 @@
 
 VERSION=1.0.${BUILD_NUMBER:-0}
 
+docker stop policy-store || true
 docker rm -f $(docker ps -a -q) || true
+
 docker run \
     --publish=27017:27017 \
     --volume $(pwd)/data/db:/data/db \
@@ -13,6 +15,9 @@ docker run \
 docker exec -it policy-store \
     bash -c "mongoimport --db policy-search-db --collection policies --drop --type json --file /input-data/policies-2018.json --jsonArray"
 docker exec -it policy-store bash -c "mongo policy-search-db /input-data/update-schema.js"
+
+docker stop policy-store || true
+docker rm -f $(docker ps -a -q) || true
 
 docker build -t policy-store .
 
